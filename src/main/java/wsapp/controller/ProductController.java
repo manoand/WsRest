fd969a0ead353b31c7015da211ef0fbe0202cb51
Manoa <manoa.andri@gmail.com>
2021-10-21 12:53:34 +0200

    Refacto request path and add getProduct

--- src/main/java/wsapp/controller/ProductController.java
+++ src/main/java/wsapp/controller/ProductController.java
@@ -1,5 +1,7 @@
 package wsapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import wsapp.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,25 +14,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/product/controller/")
 public class ProductController {
+    Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
     @Autowired
     private ProductService productService;
 
-    @GetMapping(value = "/product/controller/getDetails", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "getDetails", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Product>> getProductDetails() {
         List<Product> listProduct = new ArrayList<Product>(productService.getAll());
         return new ResponseEntity<List<Product>>(listProduct, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/product/controller/addProduct", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+    @PostMapping(value = "addProduct", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.TEXT_HTML_VALUE)
     public ResponseEntity<String> addProduct(@RequestBody Product product) {
         productService.insert(product);
         long count = product.getId();
-        return new ResponseEntity<String>("Product added successfully with id:" + count, HttpStatus.CREATED);
-
+        LOGGER.info("Product added successfully with id:" + count);
+        return new ResponseEntity<String>(String.valueOf(count), HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "/product/controller/updateProduct", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "getProduct/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Product> getProductDetails(@PathVariable String id) {
+        Product product = productService.getProduct(Integer.valueOf(id));
+        if(product == null){
+            return new ResponseEntity<Product>(product, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Product>(product, HttpStatus.OK);
+    }
+
+
+    @PutMapping(value = "updateProduct", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
         if (productService.getProduct(product.getId()) == null) {
             Product product2 = null;
@@ -41,7 +56,7 @@ public class ProductController {
         return new ResponseEntity<Product>(updated, HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "/product/controller/deleteProduct/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "deleteProduct/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Product> deleteProduct(@PathVariable("id") int myId) {
         Product product2 = productService.getProduct(myId);
         if (product2 == null) {
